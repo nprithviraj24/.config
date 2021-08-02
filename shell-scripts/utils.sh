e07dbec0e649c161ac7de8bfad65f1ff68238a06
Prithvi Raju <nprithviraj24@gmail.com>
2021-08-02 21:54:50 +0530

    Add syncRemote changes

--- shell-scripts/utils.sh
+++ shell-scripts/utils.sh
@@ -11,7 +11,21 @@ generateqr (){
 # Make sure remote desktop has rsync 
 syncRemote() {
     echo 'Watched established in folder -->' "$(pwd)"
-    while inotifywait -r -e modify,create,delete,move $(pwd); do
-        rsync -avz "$(pwd)""/"  "$1" 
-    done
+    echo "Maximum size :  200mb "
+    
+    if [[ -z $3  && -z $4 ]] 
+    then
+	    while inotifywait -r -e modify,create,delete,move $(pwd); do
+		rsync -avz --max-size=200m "$(pwd)""/"  "$1" 
+	    done
+    fi
+    if [ ! -z "$3" ] && [ ! -z "$4" ] $$ [ "$3" == "--ignore" ]
+    then
+	    while inotifywait -r -e modify,create,delete,move $(pwd); do
+		rsync -avz --exclude="$4" "$(pwd)""/"  "$1" 
+	    done
+    else
+	    echo "Usage syncRemote:abc:~/asdf --ignore *.pt"
+    fi
+
 } 
